f27d105fbd447ee8d954e58269e82fb315b0d618
Andrea Bigagli <abigagli@gmail.com>
2024-02-07 11:56:19 +0100

    Rename camke scripts

--- cmake/versioning/targets.cmake
+++ cmake/versioning/targets.cmake
@@ -4,7 +4,7 @@ add_custom_target(
     ${CMAKE_COMMAND}
     -DVERSION_TEMPLATE_FILE=${CMAKE_CURRENT_LIST_DIR}/version_git_only.c.in
     -DVERSION_SOURCE_FILE=version_git_only.c -P
-    ${CMAKE_CURRENT_LIST_DIR}/git_sha.cmake
+    ${CMAKE_CURRENT_LIST_DIR}/generate_git_describe.cmake
   BYPRODUCTS version_git_only.c)
 
 add_custom_target(
@@ -13,16 +13,13 @@ add_custom_target(
     ${CMAKE_COMMAND}
     -DVERSION_TEMPLATE_FILE=${CMAKE_CURRENT_LIST_DIR}/version_deploy.c.in
     -DVERSION_SOURCE_FILE=version_deploy.c -P
-    ${CMAKE_CURRENT_LIST_DIR}/deploy_version.cmake
+    ${CMAKE_CURRENT_LIST_DIR}/generate_deploy_version.cmake
   BYPRODUCTS version_deploy.c)
 
 macro(ADD_DEPLOY_TARGET_FOR target_name image_basename images_folder script)
   add_custom_target(
     deploy_${target_name}
-    COMMAND
-      ${script}
-      # ${images_folder} $<TARGET_FILE_DIR:${target_name}>/${target_name}.bin
-      ${images_folder} $<TARGET_FILE:${target_name}> ${image_basename}
-      ${CMAKE_BINARY_DIR}/next_deploy_version.txt
+    COMMAND ${script} ${images_folder} $<TARGET_FILE:${target_name}>
+            ${image_basename} ${CMAKE_BINARY_DIR}/next_deploy_version.txt
     DEPENDS ${target_name})
 endmacro()
